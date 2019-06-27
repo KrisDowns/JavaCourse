9a1fa1c3730e430a14b98b291b181318671572f0
Kris Downs <kdowns01@gmail.com>
2019-06-27 00:14:25 -0400

    Initial Commit

---
+++ ByteShortInt/src/com/kris/Main.java
@@ -0,0 +1,19 @@
+package com.kris;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int myValue = 1000;
+
+	// int has a width of 32
+	int myMinValue = -2_147_483_648;
+	int MyMaxValue = 2_147_483_647;
+
+	// byte has a width of 8
+	byte myByteValue = -128;
+
+	//short has a width of 16
+	short myShortValue = 32767;
+
+    }
+}
---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Kris!");
+
+        int myFirstNumber = 5;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        System.out.println(myFirstNumber);
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+        System.out.println(1000 - myTotal);
+    }
+}
